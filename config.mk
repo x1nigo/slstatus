c7898b591b8a18a36b35ca4bb9fa60603e599d62
x1nigo <109367483+x1nigo@users.noreply.github.com>
2024-05-08 22:10:38 +0800

    Add files via upload

--- config.mk
+++ config.mk
@@ -13,10 +13,10 @@ X11LIB = /usr/X11R6/lib
 # flags
 CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE -DVERSION=\"${VERSION}\"
 CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Wno-unused-parameter -Os
-LDFLAGS  = -L$(X11LIB) -s -lsndio
+LDFLAGS  = -L$(X11LIB) -s
 # OpenBSD: add -lsndio
 # FreeBSD: add -lkvm -lsndio
-LDLIBS   = -lX11
+LDLIBS   = -lX11 -lsndio
 
 # compiler and linker
 CC = cc
